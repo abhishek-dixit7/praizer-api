ea1c81a011d8665ca792799c6f7e3091151f0f0b
Abhishek <abhishekdixit9024@gmail.com>
2023-05-22 18:33:38 +0530

    added login and callback apis for google authentication.
    - need to finalize the callback action method to store the user in the db.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace praizer_api.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    [AllowAnonymous]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            // Redirect to Google OAuth login
+            return Challenge(new AuthenticationProperties { RedirectUri = "/api/auth/callback" }, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet("callback")]
+        public async Task<IActionResult> Callback()
+        {
+            // Handle the callback and obtain user details
+            var authenticateResult = await HttpContext.AuthenticateAsync();
+
+            if (!authenticateResult.Succeeded)
+            {
+                // Handle authentication failure
+                return Unauthorized();
+            }
+
+            // Generate JWT token
+            var token = GenerateJwtToken("heelo");
+
+            // Return the JWT token
+            return Ok(new { Token = token });
+        }
+
+        private string GenerateJwtToken(string userId)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Secret"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.NameIdentifier, userId),
+            }),
+                Expires = DateTime.UtcNow.AddDays(Convert.ToInt32(_configuration["Jwt:ExpirationDays"])),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuration
+builder.Configuration.AddJsonFile("appsettings.json", optional: false);
 
 // Add services to the container.
 
@@ -6,6 +11,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+}).AddCookie()
+.AddGoogle(options =>
+{
+    options.ClientId = builder.Configuration["GoogleAuth:ClientId"];
+    options.ClientSecret = builder.Configuration["GoogleAuth:ClientSecret"];
+});
+
+
 
 var app = builder.Build();
 
@@ -18,6 +37,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
