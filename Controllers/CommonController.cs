bdcb8ab97a968b5d469c4a9e8fd947f0e73ea230
shoyab8382 <shoya8382alam@gmail.com>
2023-06-26 09:46:16 +0530

    my team api binded

---
+++ Controllers/CommonController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using praizer_api.Contracts.Requests;
+using praizer_api.Services;
+
+namespace praizer_api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommonController : ControllerBase
+    {
+        [HttpGet("getTeamMembers")]
+        public async Task<IActionResult> GetTeamMembers(string uid)
+        {
+            try
+            {
+                var result = await CommonService.GetTeamMembers(uid);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e);
+                return NotFound(e);
+            }
+        }
+    }
+}
